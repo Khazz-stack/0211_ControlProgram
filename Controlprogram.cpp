ea5cc73aa078c22b5a18401580a5ddd9b65b9e86
Khazz <thelenaid@gmail.com>
2026-03-09 18:00:55 +0700

    Int main nilai

--- Controlprogram.cpp
+++ Controlprogram.cpp
@@ -4,3 +4,16 @@ using namespace std;
 float Rata_rata(float a, float n){
     return (a+b)/2;
 }
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
+
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+}
